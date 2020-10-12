31a3e95fad1182ceb84f8f80a7f118607479428c
michal <m.pietrucha@techocean.pl>
2020-10-12 15:19:08 +0200

    fix database wrong table name

--- models/UsersModel/User.js
+++ models/UsersModel/User.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('../../database/db');
 
 module.exports = db.sequelize.define(
-    'Users',
+    'users',
     {
         id: {
             type: Sequelize.INTEGER,
